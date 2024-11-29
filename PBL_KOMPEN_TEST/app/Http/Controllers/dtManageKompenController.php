f9af5df9a55e3a278b4c7b025da47dc052344280
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-11-29 17:38:12 +0700

    dosen/tendik - manage kompen

--- PBL_KOMPEN_TEST/app/Http/Controllers/dtManageKompenController.php
+++ PBL_KOMPEN_TEST/app/Http/Controllers/dtManageKompenController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ManageKompenModel;
+use Yajra\DataTables\Facades\DataTables;
 
 class dtManageKompenController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $breadcrumb = (object)[
             'title' => 'Manage Kompen',
             'list' => ['Home', 'Manage Kompen']
@@ -19,5 +22,37 @@ class dtManageKompenController extends Controller
         $activeMenu = 'dtManageKompen';
 
         return view('dtManageKompen.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+    }
+
+    public function list(Request $request)
+    {
+        // Query untuk mengambil data kompen
+        $kompen = ManageKompenModel::select(
+            'id_tugas_kompen',
+            'nama_tugas',
+            'pemberi_tugas',
+            'jenis_tugas_kompen',
+            'deskripsi',
+            'kuota',
+            'jam_kompen',
+            'status',
+            'tanggal_mulai',
+            'tanggal_selesai',
+            'id_jenis_tugas',
+            'id_bidang_kompetensi'
+        );
+
+        // Menggunakan DataTables untuk merender data
+        return DataTables::of($kompen)
+            // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($kompen) { // menambahkan kolom aksi
+                $btn = '<button onclick="modalAction(\'' . url('/dtManageKompen/' . $kompen->id_tugas_kompen . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/dtManageKompen/' . $kompen->id_tugas_kompen . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/dtManageKompen/' . $kompen->id_tugas_kompen . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+            ->make(true);
     }
 }
--- PBL_KOMPEN_TEST/resources/views/dtManageKompen/index.blade.php
+++ PBL_KOMPEN_TEST/resources/views/dtManageKompen/index.blade.php
@@ -13,24 +13,60 @@
         <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
 
-        <table class="table table-bordered table-striped table-hover table-sm">
+        <table id="kompenTable" class="table table-bordered table-striped table-hover table-sm">
             <thead>
                 <tr>
                     <th>No</th>
-                    <th>Pemberi Kompen</th>
+                    <th>Nama Tugas</th>
+                    <th>Pemberi Tugas</th>
                     <th>Jenis Tugas Kompen</th>
                     <th>Deskripsi</th>
                     <th>Kuota</th>
                     <th>Jam Kompen</th>
-                    <th>Status Dibuka</th>
-                    <th>Waktu Pengerjaan</th>
-                    <th>Tag Kompetensi</th>
+                    <th>Status</th>
+                    <th>Tanggal Mulai</th>
+                    <th>Tanggal Selesai</th>
                     <th>Aksi</th>
                 </tr>
             </thead>
+            <tbody>
+                <!-- Data akan dimuat di sini oleh JavaScript -->
+            </tbody>
         </table>
     </div>
 </div>
-<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data backdrop="static"
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
     data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+@endsection
+
+@section('scripts')
+<script>
+    $(document).ready(function() {
+        // Inisialisasi DataTable
+        $('#kompenTable').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: '{{ url('dtManageKompen/list') }}', // URL untuk mengambil data
+                type: 'POST',
+                data: function (d) {
+                    // Menambahkan data tambahan untuk dikirim ke server, jika diperlukan
+                }
+            },
+            columns: [
+                { data: 'DT_RowIndex', name: 'DT_RowIndex' }, // Menambahkan nomor urut
+                { data: 'nama_tugas', name: 'nama_tugas' }, // Kolom Nama Tugas
+                { data: 'pemberi_tugas', name: 'pemberi_tugas' }, // Kolom Pemberi Tugas
+                { data: 'jenis_tugas_kompen', name: 'jenis_tugas_kompen' }, // Kolom Jenis Tugas Kompen
+                { data: 'deskripsi', name: 'deskripsi' }, // Kolom Deskripsi
+                { data: 'kuota', name: 'kuota' }, // Kolom Kuota
+                { data: 'jam_kompen', name: 'jam_kompen' }, // Kolom Jam Kompen
+                { data: 'status', name: 'status' }, // Kolom Status
+                { data: 'tanggal_mulai', name: 'tanggal_mulai' }, // Kolom Tanggal Mulai
+                { data: 'tanggal_selesai', name: 'tanggal_selesai' }, // Kolom Tanggal Selesai
+                { data: 'aksi', name: 'aksi', orderable: false, searchable: false } // Kolom Aksi
+            ]
+        });
+    });
+</script>
 @endsection
