d653848764dfb37c8ecaf4c3bc03374e7813c40d
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-12-05 22:12:09 +0700

    Dashboard Dosen Teknisi

--- PBL_KOMPEN_TEST/app/Http/Controllers/dtWelcomeController.php
+++ PBL_KOMPEN_TEST/app/Http/Controllers/dtWelcomeController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class dtWelcomeController extends Controller
 {
-    //
+    public function index(){
+        $breadcrumb = (object)[
+            'title' => 'Dashboard',
+            'list' => ['Home', 'Welcome']
+        ];
+
+        $activeMenu = 'dashboard';
+
+        return view('dt_welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+    }
 }
